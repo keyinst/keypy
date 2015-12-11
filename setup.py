10a74105a91ec01610075b173968b674e10e11cc
Patricia Milz <patricia.milz@key.uzh.ch>
2015-12-11 09:48:04 +0100

    Release 1.1

--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
-#!/usr/bin/env python
+﻿#!/usr/bin/env python
 
 from distutils.core import setup
 
 setup(name='keypy',
-      version='1.0',
+      version='1.1',
       description='The KEY Institute for Brain-Mind Research - EEG Analysis Toolbox',
       author='Patricia Milz',
       author_email='patricia.milz@key.uzh.ch',
       url='https://github.com/keyinst/keypy',
       license='GPLv3',
       packages=['keypy', 'keypy.microstates', 'keypy.preprocessing', 'keypy.signal'],
-      install_requires = ["numpy", "scipy", "h5py"]
+      install_requires = ["numpy", "scipy", "h5py"],
+      classifiers=[
+          'Development Status :: 4 - Beta'
+      ]
      )
