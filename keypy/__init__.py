5be6c6c7ddc8206a5233609dba0bed54f34b6454
Patricia Milz <patricia.milz@key.uzh.ch>
2016-04-03 19:36:07 +0200

    cleaning up

--- keypy/__init__.py
+++ keypy/__init__.py
@@ -6,53 +6,7 @@ The KEY EEG Analysis Toolbox
 The KEY EEG Analysis Toolbox includes functionality for EEG preprocessing, microstate analysis, spectral analysis, and statistics.
 """
 
-#################
-# 0.) IMPORT PACKAGES
-#################
-
-import numpy
-from numpy.linalg.linalg import eig
-from numpy.core.fromnumeric import size, argsort
-
-#################
-# 0.) DEFINE FUNCTIONS
-#################
-
-
-def princomp_C(A,numpc=0):
-    """
-    Compute 1st Principal Component.
-	Function modified from: http://glowingpython.blogspot.ch/2011/07/principal-component-analysis-with-numpy.html
-
-    Parameters
-    ----------
-    A : object of type MstConfiguration
-        Contains the following attributes: subtract_column_mean_at_start (bool), debug (bool), use_gfp_peaks (bool), force_avgref (bool), set_gfp_all_1 (bool), use_smoothing (bool), gfp_type_smoothing (string),
-        smoothing_window (int), use_fancy_peaks (bool), method_GFPpeak (string), original_nr_of_maps (int), seed_number (int), max_number_of_iterations (int), ERP (bool), correspondance_cutoff (double):
-    numpc : int
-		number of principal components
-
-    Returns
-    -------
-        coeff: array
-            first principal component
-		idx: 
-
-		latent: 
-
-    """
-    M=A.T
-    a=numpy.dot(M,M.T) #get covariance matrix by  matrix multiplication of data with transposed data
-    [latent,coeff]=eig(a)
-    p = size(coeff,axis=1)
-    idx = argsort(latent) # sorting the eigenvalues
-    idx = idx[::-1]       # in ascending order
-    # sorting eigenvectors according to the sorted eigenvalues
-    coeff = coeff[:,idx]
-    #latent = latent[idx] # sorting eigenvalues
-    if numpc < p or numpc >= 0:
-        coeff = coeff[:,list(range(numpc))] # cutting some PCs
-    #score = dot(coeff.T,M) # projection of the data in the new space
-    return coeff, idx, latent
-
-
+import io
+import microstates
+import preprocessing
+import signal
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='keypy',
-      version='1.2',
+      version='1.2.1',
       description='The KEY Institute for Brain-Mind Research - EEG Analysis Toolbox',
       author='Patricia Milz',
       author_email='patricia.milz@key.uzh.ch',
@@ -12,6 +12,6 @@ setup(name='keypy',
       packages=['keypy', 'keypy.microstates', 'keypy.preprocessing', 'keypy.signal','keypy.io'],
       install_requires = ["numpy", "scipy", "h5py"],
       classifiers=[
-          'Development Status :: 5 - Stable'
+          'Development Status :: 4 - Beta'
       ]
      )
