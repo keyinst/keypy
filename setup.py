3e438c878667f8626877fbed11b550f3b9132f06
Patricia Milz <patricia.milz@key.uzh.ch>
2016-07-21 16:20:30 +0200

    Update setup.py for new release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='keypy',
-      version='2.0.0',
+      version='2.0.1',
       description='The KEY Institute for Brain-Mind Research - EEG Analysis Toolbox',
       author='Patricia Milz',
       author_email='patricia.milz@key.uzh.ch',
