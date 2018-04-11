1a1e7e273e9473aa64bd54a00d9407e6d095d049
Patricia Milz <Ascronia@users.noreply.github.com>
2018-04-11 14:20:59 +0200

    Updating Setup.py for Release 2.0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='keypy',
-      version='2.0.1',
+      version='2.0.2',
       description='The KEY Institute for Brain-Mind Research - EEG Analysis Toolbox',
       author='Patricia Milz',
       author_email='patricia.milz@key.uzh.ch',
